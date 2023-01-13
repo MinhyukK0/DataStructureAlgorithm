f2abe1033e7b351cd8be891d71500a909e2682dc
MinhyukK0 <88814888+MinhyukK0@users.noreply.github.com>
2023-01-13 16:18:19 +0900

    Time: 131 ms (78.27%), Space: 16.1 MB (18.23%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,17 +1,33 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        if not height or len(height)<3:
-            return 0
-        left, right = 0, len(height)-1
-        l_max, r_max = height[left], height[right]
+        # solution 1: pointer
+        # if not height or len(height)<3:
+        #     return 0
+        # left, right = 0, len(height)-1
+        # l_max, r_max = height[left], height[right]
+        # vol = 0
+        # while left < right:
+        #     l_max = max(l_max, height[left])
+        #     r_max = max(r_max, height[right])
+        #     if l_max < r_max:
+        #         vol += l_max - height[left]
+        #         left += 1
+        #     else:
+        #         vol += r_max - height[right]
+        #         right -= 1
+        # return vol
+        
+        # solution 2: stack
+        stack = []
         vol = 0
-        while left < right:
-            l_max = max(l_max, height[left])
-            r_max = max(r_max, height[right])
-            if l_max < r_max:
-                vol += l_max - height[left]
-                left += 1
-            else:
-                vol += r_max - height[right]
-                right -= 1
+        for i in range(len(height)):
+            while stack and height[i] > height[stack[-1]]:
+                top = stack.pop()
+                if not stack:
+                    break
+                width = i - stack[-1] -1
+                h = min(height[i], height[stack[-1]]) - height[top]
+                vol += width * h
+            stack.append(i)
+        
         return vol
